66810bf8f1b6b38374f1ceed7e8cbc5767921b8c
mateus <mateus.tigreewallet@gmail.comgit config --global user.email mateus.tigreewallet@gmail.com>
2022-06-12 00:44:23 -0300

    confirmação do pedido(feito)

--- DrivenEats.js
+++ DrivenEats.js
@@ -1,26 +1,53 @@
+let cont = 0;
+
 function pudim(pudim) {
   const bord = document.querySelector(".pudim.selected");
-  if (bord !== null) {
+  if (bord) {
     bord.classList.remove("selected");
+    cont--;
   }
 
   pudim.classList.add("selected");
+  cont++;
+
+  confirmed();
 }
 
 function cola(cola) {
   const bord = document.querySelector(".cola.selected");
-  if (bord !== null) {
+  if (bord) {
     bord.classList.remove("selected");
+    cont--;
   }
 
   cola.classList.add("selected");
+  cont++;
+
+  confirmed();
 }
 
-function chickey(chickey) {
-  const bord = document.querySelector(".chickey.selected");
-  if (bord !== null) {
+function chicken(chicken) {
+  const bord = document.querySelector(".chicken.selected");
+  if (bord) {
     bord.classList.remove("selected");
+    cont--;
   }
 
-  chickey.classList.add("selected");
+  chicken.classList.add("selected");
+  cont++;
+
+  confirmed();
+}
+
+function confirmed() {
+  if (cont === 3) {
+    const confirmed = document.querySelector(".confirmacao");
+    confirmed.classList.add("confirmed");
+
+    const teste = document.querySelector(".testando");
+    teste.classList.remove("testando");
+
+    const esconder = document.querySelector(".esconder");
+    esconder.classList.add("testando");
+  }
 }
